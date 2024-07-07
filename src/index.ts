8d992478cbd130c620086d86911657a8cfa7a069
Markus Kurzmann <markus@kurzmann.io>
2024-07-07 15:25:30 +0200

    Also export the IOnChangeTrigger type definition

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { Virtuosa } from "$/components/Virtuosa";
 import { IVirtuosaElement } from "$/types/IVirtuosaElement";
 import { IVisibilityStatus } from "$/types/IVisibilityStatus";
+import { IOnChangeTrigger } from "$/types/IOnChangeTrigger";
 
 export { Virtuosa };
-export type { IVirtuosaElement, IVisibilityStatus };
+export type { IOnChangeTrigger, IVirtuosaElement, IVisibilityStatus };
